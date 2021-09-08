1e421b18828273be9c6493dbfa95c1c007a2b2f8
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-09-08 23:20:59 +0200

    add table to height doc page

---
+++ docs/utilities/sizing/heightElements.js
@@ -0,0 +1,246 @@
+export default [
+  {
+    class: "h-0",
+    value: "{ height: 0 }",
+  },
+  {
+    class: "h-0.25",
+    value: "{ height: 1 }",
+  },
+  {
+    class: "h-0.5",
+    value: "{ height: 2 }",
+  },
+  {
+    class: "h-1",
+    value: "{ height: 4 }",
+  },
+  {
+    class: "h-1.5",
+    value: "{ height: 6 }",
+  },
+  {
+    class: "h-2",
+    value: "{ height: 8 }",
+  },
+  {
+    class: "h-2.5",
+    value: "{ height: 10 }",
+  },
+  {
+    class: "h-3",
+    value: "{ height: 12 }",
+  },
+  {
+    class: "h-3.5",
+    value: "{ height: 14 }",
+  },
+  {
+    class: "h-4",
+    value: "{ height: 16 }",
+  },
+  {
+    class: "h-5",
+    value: "{ height: 20 }",
+  },
+  {
+    class: "h-6",
+    value: "{ height: 24 }",
+  },
+  {
+    class: "h-7",
+    value: "{ height: 28 }",
+  },
+  {
+    class: "h-8",
+    value: "{ height: 32 }",
+  },
+  {
+    class: "h-9",
+    value: "{ height: 36 }",
+  },
+  {
+    class: "h-10",
+    value: "{ height: 40 }",
+  },
+  {
+    class: "h-11",
+    value: "{ height: 44 }",
+  },
+  {
+    class: "h-12",
+    value: "{ height: 48 }",
+  },
+  {
+    class: "h-14",
+    value: "{ height: 56 }",
+  },
+  {
+    class: "h-16",
+    value: "{ height: 64 }",
+  },
+  {
+    class: "h-20",
+    value: "{ height: 80 }",
+  },
+  {
+    class: "h-24",
+    value: "{ height: 96 }",
+  },
+  {
+    class: "h-28",
+    value: "{ height: 112 }",
+  },
+  {
+    class: "h-32",
+    value: "{ height: 128 }",
+  },
+  {
+    class: "h-36",
+    value: "{ height: 144 }",
+  },
+  {
+    class: "h-40",
+    value: "{ height: 160 }",
+  },
+  {
+    class: "h-44",
+    value: "{ height: 172 }",
+  },
+  {
+    class: "h-48",
+    value: "{ height: 196 }",
+  },
+  {
+    class: "h-52",
+    value: "{ height: 208 }",
+  },
+  {
+    class: "h-56",
+    value: "{ height: 224 }",
+  },
+  {
+    class: "h-60",
+    value: "{ height: 240 }",
+  },
+  {
+    class: "h-64",
+    value: "{ height: 256 }",
+  },
+  {
+    class: "h-72",
+    value: "{ height: 288 }",
+  },
+  {
+    class: "h-80",
+    value: "{ height: 320 }",
+  },
+  {
+    class: "h-1/2",
+    value: "{ height: '50%' }",
+  },
+  {
+    class: "h-1/3",
+    value: "{ height: '33.333333%' }",
+  },
+  {
+    class: "h-2/3",
+    value: "{ height: '66.666667%' }",
+  },
+  {
+    class: "h-1/4",
+    value: "{ height: '25%' }",
+  },
+  {
+    class: "h-2/4",
+    value: "{ height: '50%' }",
+  },
+  {
+    class: "h-3/4",
+    value: "{ height: '75%' }",
+  },
+  {
+    class: "h-1/5",
+    value: "{ height: '20%' }",
+  },
+  {
+    class: "h-2/5",
+    value: "{ height: '40%' }",
+  },
+  {
+    class: "h-3/5",
+    value: "{ height: '60%' }",
+  },
+  {
+    class: "h-4/5",
+    value: "{ height: '80%' }",
+  },
+  {
+    class: "h-1/6",
+    value: "{ height: '16.666667%' }",
+  },
+  {
+    class: "h-2/6",
+    value: "{ height: '33.333333%' }",
+  },
+  {
+    class: "h-3/6",
+    value: "{ height: '50%' }",
+  },
+  {
+    class: "h-4/6",
+    value: "{ height: '66,666667%' }",
+  },
+  {
+    class: "h-5/6",
+    value: "{ height: '83.333333%' }",
+  },
+  {
+    class: "h-1/12",
+    value: "{ height: '8.333333%' }",
+  },
+  {
+    class: "h-2/12",
+    value: "{ height: '16.666667%' }",
+  },
+  {
+    class: "h-3/12",
+    value: "{ height: '25%' }",
+  },
+  {
+    class: "h-4/12",
+    value: "{ height: '33.333333%' }",
+  },
+  {
+    class: "h-5/12",
+    value: "{ height: '41.666667%' }",
+  },
+  {
+    class: "h-6/12",
+    value: "{ height: '50%' }",
+  },
+  {
+    class: "h-7/12",
+    value: "{ height: '58.333333%' }",
+  },
+  {
+    class: "h-8/12",
+    value: "{ height: '66.666667%' }",
+  },
+  {
+    class: "h-9/12",
+    value: "{ height: '75%' }",
+  },
+  {
+    class: "h-10/12",
+    value: "{ height: '83.333333%' }",
+  },
+  {
+    class: "h-11/12",
+    value: "{ height: '91.666667%' }",
+  },
+  {
+    class: "h-full",
+    value: "{ height: '100%' }",
+  },
+];
