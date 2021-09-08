3f6cfac81a7871e9b22cd787eb3b66334895c873
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-09-08 23:38:25 +0200

    add width properties

---
+++ docs/utilities/sizing/widthElements.js
@@ -0,0 +1,246 @@
+export default [
+  {
+    class: "w-0",
+    value: "{ width: 0 }",
+  },
+  {
+    class: "w-0.25",
+    value: "{ width: 1 }",
+  },
+  {
+    class: "w-0.5",
+    value: "{ width: 2 }",
+  },
+  {
+    class: "w-1",
+    value: "{ width: 4 }",
+  },
+  {
+    class: "w-1.5",
+    value: "{ width: 6 }",
+  },
+  {
+    class: "w-2",
+    value: "{ width: 8 }",
+  },
+  {
+    class: "w-2.5",
+    value: "{ width: 10 }",
+  },
+  {
+    class: "w-3",
+    value: "{ width: 12 }",
+  },
+  {
+    class: "w-3.5",
+    value: "{ width: 14 }",
+  },
+  {
+    class: "w-4",
+    value: "{ width: 16 }",
+  },
+  {
+    class: "w-5",
+    value: "{ width: 20 }",
+  },
+  {
+    class: "w-6",
+    value: "{ width: 24 }",
+  },
+  {
+    class: "w-7",
+    value: "{ width: 28 }",
+  },
+  {
+    class: "w-8",
+    value: "{ width: 32 }",
+  },
+  {
+    class: "w-9",
+    value: "{ width: 36 }",
+  },
+  {
+    class: "w-10",
+    value: "{ width: 40 }",
+  },
+  {
+    class: "w-11",
+    value: "{ width: 44 }",
+  },
+  {
+    class: "w-12",
+    value: "{ width: 48 }",
+  },
+  {
+    class: "w-14",
+    value: "{ width: 56 }",
+  },
+  {
+    class: "w-16",
+    value: "{ width: 64 }",
+  },
+  {
+    class: "w-20",
+    value: "{ width: 80 }",
+  },
+  {
+    class: "w-24",
+    value: "{ width: 96 }",
+  },
+  {
+    class: "w-28",
+    value: "{ width: 112 }",
+  },
+  {
+    class: "w-32",
+    value: "{ width: 128 }",
+  },
+  {
+    class: "w-36",
+    value: "{ width: 144 }",
+  },
+  {
+    class: "w-40",
+    value: "{ width: 160 }",
+  },
+  {
+    class: "w-44",
+    value: "{ width: 172 }",
+  },
+  {
+    class: "w-48",
+    value: "{ width: 196 }",
+  },
+  {
+    class: "w-52",
+    value: "{ width: 208 }",
+  },
+  {
+    class: "w-56",
+    value: "{ width: 224 }",
+  },
+  {
+    class: "w-60",
+    value: "{ width: 240 }",
+  },
+  {
+    class: "w-64",
+    value: "{ width: 256 }",
+  },
+  {
+    class: "w-72",
+    value: "{ width: 288 }",
+  },
+  {
+    class: "w-80",
+    value: "{ width: 320 }",
+  },
+  {
+    class: "w-1/2",
+    value: "{ width: '50%' }",
+  },
+  {
+    class: "w-1/3",
+    value: "{ width: '33.333333%' }",
+  },
+  {
+    class: "w-2/3",
+    value: "{ width: '66.666667%' }",
+  },
+  {
+    class: "w-1/4",
+    value: "{ width: '25%' }",
+  },
+  {
+    class: "w-2/4",
+    value: "{ width: '50%' }",
+  },
+  {
+    class: "w-3/4",
+    value: "{ width: '75%' }",
+  },
+  {
+    class: "w-1/5",
+    value: "{ width: '20%' }",
+  },
+  {
+    class: "w-2/5",
+    value: "{ width: '40%' }",
+  },
+  {
+    class: "w-3/5",
+    value: "{ width: '60%' }",
+  },
+  {
+    class: "w-4/5",
+    value: "{ width: '80%' }",
+  },
+  {
+    class: "w-1/6",
+    value: "{ width: '16.666667%' }",
+  },
+  {
+    class: "w-2/6",
+    value: "{ width: '33.333333%' }",
+  },
+  {
+    class: "w-3/6",
+    value: "{ width: '50%' }",
+  },
+  {
+    class: "w-4/6",
+    value: "{ width: '66,666667%' }",
+  },
+  {
+    class: "w-5/6",
+    value: "{ width: '83.333333%' }",
+  },
+  {
+    class: "w-1/12",
+    value: "{ width: '8.333333%' }",
+  },
+  {
+    class: "w-2/12",
+    value: "{ width: '16.666667%' }",
+  },
+  {
+    class: "w-3/12",
+    value: "{ width: '25%' }",
+  },
+  {
+    class: "w-4/12",
+    value: "{ width: '33.333333%' }",
+  },
+  {
+    class: "w-5/12",
+    value: "{ width: '41.666667%' }",
+  },
+  {
+    class: "w-6/12",
+    value: "{ width: '50%' }",
+  },
+  {
+    class: "w-7/12",
+    value: "{ width: '58.333333%' }",
+  },
+  {
+    class: "w-8/12",
+    value: "{ width: '66.666667%' }",
+  },
+  {
+    class: "w-9/12",
+    value: "{ width: '75%' }",
+  },
+  {
+    class: "w-10/12",
+    value: "{ width: '83.333333%' }",
+  },
+  {
+    class: "w-11/12",
+    value: "{ width: '91.666667%' }",
+  },
+  {
+    class: "w-full",
+    value: "{ width: '100%' }",
+  },
+];
