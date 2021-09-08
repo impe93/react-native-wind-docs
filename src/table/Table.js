eae12956a6dc3f04329941fd812cc55a6153e348
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-09-08 23:20:37 +0200

    add table component

--- src/table/Table.js
+++ src/table/Table.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import clsx from 'clsx'
-import styles from './table.module.css'
+import React, { useMemo } from "react";
+import clsx from "clsx";
+import styles from "./table.module.css";
 
-export default ({ tableName }) => {
-  return <div className={clsx("card", styles.table)}>
-    <div className="card__header">
-      <h3>{tableName}</h3>
-    </div>
-  </div>
-}
+export default ({ elements }) => {
+  const toShow = useMemo(() => {
+    return (
+      <>
+        {elements.map((e, i) => {
+          return (
+            <div key={i} className={clsx(styles.line, styles.code)}>
+              <div className={clsx('f-1', styles.classUtility)}>{e.class}</div>
+              <div className="f-2">{e.value}</div>
+            </div>
+          );
+        })}
+      </>
+    );
+  }, []);
+
+  return (
+    <>
+      <div className={clsx(styles.line, styles.tableHeader)}>
+        <div className="f-1">Class</div>
+        <div className="f-2">Properties</div>
+      </div>
+      <div className={styles.table}>
+        <div>{toShow}</div>
+      </div>
+    </>
+  );
+};
